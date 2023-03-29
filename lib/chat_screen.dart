fa1fff0a41968c93efdffc4c3d9ea6f426a82a63
Jay Bhararia <jaybhararia@gmail.com>
2023-03-29 22:37:52 +0530

    Copy to Clipboard Function Implemented

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -8,6 +8,7 @@ import 'constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+// import 'dart:ffi';
 
 class Chat extends StatefulWidget {
 
